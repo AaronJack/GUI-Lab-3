5049f9d2cc6e4d5edd7182753feec716a74965c4
AaronJ <ajack9601@bths.edu>
2019-01-04 11:45:43 -0500

    2nd Version

--- src/com/company/fast_clicker.java
+++ src/com/company/fast_clicker.java
@@ -1,4 +1,45 @@
 package com.company;
 
-public class fast_clicker {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import java.lang.*;
+
+public class fast_clicker extends Application  {
+
+    long start = System.nanoTime();
+    int numclicked = 0;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("HBox Experiment 1");
+
+        Label label = new Label("Not clicked");
+        Button button = new Button("Click");
+
+        button.setOnAction(value ->  {
+            label.setText("Keep Clicking");
+            numclicked++;
+            if((System.nanoTime() - start)/1000000000 >= 10){
+                System.out.println(numclicked);
+                primaryStage.close();
+            }
+        });
+
+        HBox hbox = new HBox(button, label);
+
+        Scene scene = new Scene(hbox, 200, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 }
